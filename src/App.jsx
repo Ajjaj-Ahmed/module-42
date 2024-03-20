e0cd2b49e712e541ae27b09f80ce6e1df15bce03
Ajjaj-Ahmed <ajjajahmedasif@gmail.com>
2024-03-20 10:44:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import NavBar from './Components/NavBar/NavBar'
+function App() {
+ 
+
+  return (
+    <>
+    <DaisyNav></DaisyNav>
+    <h1 className='text-4xl bg-pink-200 p-6'>React Fundamental</h1>
+    <NavBar></NavBar>
+      
+      
+   
+    </>
+  )
+}
+
+export default App
